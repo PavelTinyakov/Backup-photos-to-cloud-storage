d45b454b8963339d17c1e86ff783e98ffe66f847
paveltinyakov <tinyakov.p.g@yandex.ru>
2022-07-18 23:40:06 +0300

    added a progress bar, upload data records to a json file, code refactoring

--- yadisk.py
+++ yadisk.py
@@ -1,5 +1,8 @@
-import requests
+import json
 from datetime import datetime
+
+import requests
+from tqdm import tqdm
 
 
 class YandexDisk:
@@ -16,23 +19,31 @@ class YandexDisk:
     def _create_folder(self, vk_owner_id: int) -> str:
         url = self._base_url + 'resources'
         base_folder = '/VK_photos_upload'
-        response_base_folder = requests.get(url, headers=self.headers, params={'path': base_folder})
-        response_base_folder.raise_for_status()
-        if response_base_folder.status_code == 404:
+        status_base_folder = requests.get(url, headers=self.headers, params={'path': base_folder}).status_code
+        if status_base_folder == 404:
             requests.put(url, headers=self.headers, params={'path': base_folder})
         folder_name = f'ID{vk_owner_id}_{datetime.now().strftime("%y%m%d_%H-%M-%S")}'
         path = f'{base_folder}/{folder_name}'
-        requests.put(url, headers=self.headers, params={'path': path})
+        response = requests.put(url, headers=self.headers, params={'path': path})
+        response.raise_for_status()
         return path
 
-    def upload_files_to_disk(self, photos: list[dict], vk_owner_id: int):
+    def upload_files_to_disk(self, photos: list[dict], vk_owner_id: int, upload_log: bool):
         path = self._create_folder(vk_owner_id)
         url = self._base_url + 'resources/upload'
-        for photo in photos:
+        pbar = tqdm(photos, ncols=100)
+        for photo in pbar:
+            pbar.set_description(f'Я.Диск. Загрузка фото {photo["file_name"]}')
             params = {
-                'url': photo["url"],
+                'url': photo["origin_url"],
                 'path': f'{path}/{photo["file_name"]}',
                 'disable_redirects': 'true'
             }
             response = requests.post(url, params=params, headers=self.headers)
             response.raise_for_status()
+        tqdm.write('Загрузка фотографий на Я.Диск завершена')
+        if upload_log:
+            file_name = f'{path.split("/")[-1]}.json'
+            with open(f'ya_upload_log/{file_name}', 'w', encoding='utf-8') as file:
+                json.dump({path: photos}, file, indent=1)
+            tqdm.write(f'Данные загрузки записаны в файл {file_name}')
